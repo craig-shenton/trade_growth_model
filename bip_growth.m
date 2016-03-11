38538a262c2797d76ef9e0a5ff7c46d7bdb5a588
Craig Shenton <c.shenton@surrey.ac.uk>
2016-03-11 09:23:30 +0000

    check edge attachment

--- bip_growth.m
+++ bip_growth.m
@@ -1,20 +1,20 @@
 %% Bipartite Growth Model
 
 % model parameters
-time = 10000;            % number of time periods
-lambda = 1;              % vertex attachment probability
-ini_pref = 0.1;          % initial preference, must be > 0
-m = 1;                   % nunber of edges per new node
+time = 10000;               % number of time periods
+lambda = 0.05;              % vertex attachment probability
+ini_pref = 0.1;             % initial preference, must be > 0
+m = 1;                      % nunber of edges per new node
 
 % inistialise graph
-u = m;                   % number of nodes in U
-v = m;                   % number of nodes in V
-G = zeros(time+1,time+1);    % create seed network
+u = m;                      % number of nodes in U
+v = m;                      % number of nodes in V
+G = zeros(time+1,time+1);   % create seed network
 M = magic(m);
 M(M <= m^2-m) = 0;
 M(M > m^2-m) = 1;
 G(1:m,1:m) = M;
-sumedges = sum(sum(G));  % starting edges
+sumedges = sum(sum(G));     % starting edges
 
 % start time
 t = m;
@@ -42,7 +42,7 @@ while t < time
             node_u = ceil(rand * u);                 
             % add node to V 
             v = v + 1;
-            % select node m in V
+            % select node v in V
             node_v = v;
             % add edge from node_v to node_u
             G(node_v,node_u) = 1;
@@ -80,7 +80,8 @@ x2 = reshape(x2,numel(x2),1); % reshape input vector
 ccdf = 1-cdfun;
 prob_df2 = [q ccdf flo fup];
 
-% plot both degree distributions (loglog)
+%% plot both degree distributions (loglog)
+
 figure
 hold on
 h(5) = stairs(prob_df(:,1),prob_df(:,2),'b-','LineWidth',1);
@@ -95,7 +96,7 @@ for i = 1:4
     set(y2, 'IconDisplayStyle', 'off')
 end
 legend('Exporters','Importers')
-title('Model Paramerters, \lambda = 1, m = 1')
+title('Model Paramerters, \lambda = 0.05, m = 1')
 set(gca,'XLim',([0 1000]));
 set(gca,'YLim',([0.001 1]));
 set(gca,'yscale','log')
@@ -107,7 +108,63 @@ box on
 axis square
 set(gcf, 'Position', [10 10 400 400])
 set(gcf, 'Color', 'w');
-export_fig 'C:\Users\craigrshenton\Desktop\Dropbox\thesis\4_results\bipartite_gen_model\img\model_lambda_1.eps' ...
+export_fig 'C:\Users\craigrshenton\Desktop\Dropbox\thesis\4_results\bipartite_gen_model\img\model_lambda_005.eps' ...
     -eps;                                            
-export_fig 'C:\Users\craigrshenton\Desktop\Dropbox\thesis\4_results\bipartite_gen_model\img\model_lambda_1.png' ...
+export_fig 'C:\Users\craigrshenton\Desktop\Dropbox\thesis\4_results\bipartite_gen_model\img\model_lambda_005.png' ...
     -png;
+
+%% model attachment rate
+
+out = [out_degree1(1:size(out_degree,1)) out_degree];
+in = [in_degree1(1:size(out_degree,1))' in_degree'];
+
+figure
+subplot(2,1,1)
+% h(1) = fill(X,Y,'r','EdgeColor','none');
+% set(h(1),'facealpha',.2)
+%h(2) = plot(Out_all(:,2),Out_all(:,1),'+');
+[pd,gof,output] = fit(out(:,2),out(:,1),'poly2');
+coeffs = coeffvalues(pd);
+eq = 'A_k = a*k^2 + b*k + c';
+h = plot(pd,out(:,2),out(:,1),'+');
+% coeffs = polyfit(Out_all(:,2),Out_all(:,1), 2);
+% fittedX = linspace(min(Out_all(:,2)), max(Out_all(:,2)), length(Out_all(:,1)));
+% fittedY = polyval(coeffs, fittedX);
+% h(3) = plot(fittedX, fittedY,'-','linewidth',1,'Color','r');
+leg_info = get(h, 'Annotation');
+y2 = get(leg_info{1}, 'LegendInformation');
+set(y2, 'IconDisplayStyle', 'off')
+legend(eq,'location','southeast')
+t=textLoc(sprintf('Adj R^{2} = %.2f\na = %.2f\nb = %.2f\nc = %.2f',...
+    gof.adjrsquare,coeffs(1),coeffs(2),coeffs(3)),{'NorthWest',1/50},'FontSize',8);
+xlabel('k','FontSize',11)
+ylabel('A_{k}','FontSize',11)
+title('Exporters')
+set(gca,'XLim',([0 200]));
+set(gca,'YLim',([0 400]));
+%set(gca,'Xtick',0:10:120)
+%set(gca,'Ytick',0:10:50)
+grid on
+box on
+%axis square
+
+subplot(2,1,2)
+hold on
+h(1) = plot(in(:,2),in(:,1),'+');
+xlabel('k','FontSize',11)
+ylabel('A_{k}','FontSize',11)
+title('Importers')
+set(gca,'XLim',([0 200]));
+set(gca,'YLim',([0 400]));
+%set(gca,'Xtick',0:10:120)
+%set(gca,'Ytick',0:10:50)
+grid on
+box on
+%axis square
+
+set(gcf, 'Position', [10 10 800 700])
+set(gcf, 'Color', 'w');
+%export_fig 'C:\Users\craigrshenton\Desktop\Dropbox\thesis\5_results\bipartite_gen_model\img\attachment_rate.eps' ...
+    %-eps;                                            
+%export_fig 'C:\Users\craigrshenton\Desktop\Dropbox\thesis\5_results\bipartite_gen_model\img\attachment_rate.png' ...
+    %-png;
